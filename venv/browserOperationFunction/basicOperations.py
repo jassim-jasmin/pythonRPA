84d6730daec29b69f2642d8cb6328efc9a0c724f
jassim <incubator482@gmail.com>
2019-10-29 13:20:16 +0530

    modified googleSerach

--- venv/browserOperationFunction/basicOperations.py
+++ venv/browserOperationFunction/basicOperations.py
@@ -1,12 +1,30 @@
 class BasicOptions:
     def buttonClick(self, butonId):
-        browser.find_element_by_xpath("//button[@id ='" + butonId + "']").click()
+        try:
+            browser.find_element_by_xpath("//button[@id ='" + butonId + "']").click()
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def findInputAndInsert(self, fieldIdName, inputValue):
-        a = browser.find_element_by_xpath("//input[@id ='" + fieldIdName + "']")
-        a.send_keys(inputValue)
+        try:
+            a = browser.find_element_by_xpath("//input[@id ='" + fieldIdName + "']")
+            a.send_keys(inputValue)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def loginAdmin(self):
-        findInputAndInsert('username', 'admin')
-        findInputAndInsert('password', 'donotusethispassword')
-        buttonClick('login')
+        try:
+            findInputAndInsert('username', 'admin')
+            findInputAndInsert('password', 'donotusethispassword')
+            buttonClick('login')
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def searchGoogle(self,text):
+        return "https://www.google.com/search?client=ubuntu&channel=fs&q=" +text+ "&ie=utf-8&oe=utf-8"
