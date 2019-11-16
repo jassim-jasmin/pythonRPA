d829141eda9ecfa3cab4945e237529346c7ba154
jassim <incubator482@gmail.com>
2019-11-16 10:41:56 +0530

    16.1

--- venv/ExceptionHandling/webException.py
+++ venv/ExceptionHandling/webException.py
@@ -1,3 +1,6 @@
 class ExceptionHandling:
     def geckodriverException(self):
         print('geckodriverException')
+        print('wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz')
+        print('rm geckodriver')
+        print('tar -xvzf geckodriver-v0.20.0-linux64.tar.gz')
