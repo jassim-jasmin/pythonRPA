f5e32192befbbcdfd6ffe1280f494ac5b808c352
jassim <incubator482@gmail.com>
2019-12-11 12:19:10 +0530

    image processing added

--- venv/imageProcessing/imageProcessing.py
+++ venv/imageProcessing/imageProcessing.py
@@ -1,35 +1,138 @@
 import cv2
 import numpy as np
+import json
 
-def ocrImage(imageName, imageExtension, ocrFileName, filePath):
-    try:
-        from PIL import Image
-    except ImportError:
-        import Image
-    import pytesseract
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import pandas
 
-    try:
-        # fp = open('../ocrText/' + ocrFileName,'w')
-        fp = open(filePath + ocrFileName, 'w')
-    except Exception as e:
-        import os
+pathFile = open('../path.json', 'r')
+path = json.loads(pathFile.read())
+pathFile.close()
 
-        os.mkdir(filePath)
-        fp = open(filePath + ocrFileName, 'w')
+class imageProcessing():
+    """
+    image image data
+    """
+    def getOcr(self, image):
+        import pytesseract
+        try:
+            return pytesseract.image_to_string(image)
+        except Exception as e:
+            print('Exception in imageProcessing getOcr ',e)
+            return False
 
-    try:
-        fp.write(pytesseract.image_to_string(filePath + imageName + '.' + imageExtension))
-        fp.close()
-    except Exception as e:
-        print('Error in ocrImage', e)
-        return False
+    """
+    image @image data
+    return valid non empty text
+    """
+    def getValidOcr(self, image):
+        try:
+            if image.any():
+                invalidCondition = ['']
+                ocrText = pytesseract.image_to_string(image)
 
-"""
-image @opencv image object
-returns contours or False when error occured
-"""
-def getContours(image):
-    try:
+                for invalid in invalidCondition:
+                    if ocrText == invalid:
+                        return False
+                return ocrText
+            else:
+                return False
+        except Exception as e:
+            print('Error in imageProcessing getValidOcr() ', e)
+            return False
+
+    def ocrImage(self, imageName, imageExtension, ocrFileName, filePath):
+        try:
+            # fp = open('../ocrText/' + ocrFileName,'w')
+            fp = open(filePath + ocrFileName, 'w')
+        except Exception as e:
+            import os
+
+            os.mkdir(filePath)
+            fp = open(filePath + ocrFileName, 'w')
+
+        try:
+            fp.write(pytesseract.image_to_string(filePath + imageName + '.' + imageExtension))
+            fp.close()
+        except Exception as e:
+            print('Error in ocrImage', e)
+            return False
+
+    """
+    image @opencv image object
+    returns contours or False when error occured
+    """
+    def getContours(self, image):
+        try:
+            # Grayscale
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # Find Canny edges
+            edged = cv2.Canny(gray, 30, 200)
+            cv2.waitKey(0)
+
+            # Finding Contours
+            # Use a copy of the imageName e.g. edged.copy()
+            # since findContours alters the imageName
+            contours, hierarchy = cv2.findContours(edged,
+                                                   cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            return contours
+        except Exception as e:
+            print('Error in getContours', e)
+            return False
+
+    """
+    contours @array
+    contorIndex @num data contain index of contours
+    imagePath @string path to the image file
+    """
+    def cropContors(self, contours, contorIndex, imagePath):
+        try:
+            if contours:
+                idx = contorIndex
+                try:
+                    image = cv2.imread(imagePath, 0)
+                    mask = np.zeros_like(image)  # Create mask where white is what we want, black otherwise
+                    cv2.drawContours(mask, contours, idx, 255, -1)  # Draw filled contour in mask
+                    out = np.zeros_like(image)  # Extract out the object and place into output imageName
+                    out[mask == 255] = image[mask == 255]
+
+                    # Now crop
+                    (y, x) = np.where(mask == 255)
+                    (topy, topx) = (np.min(y), np.min(x))
+                    (bottomy, bottomx) = (np.max(y), np.max(x))
+                    out = out[topy:bottomy + 1, topx:bottomx + 1]
+
+                    return out
+                except Exception as e:
+                    # print('Error in cropContors imread',e)
+                    return False
+            else:
+                return False
+        except Exception as e:
+            print('Error in cropContors main',e)
+            return False
+
+    def drawContours(self, imageName, contors, contourldx=-1, color = (0, 255, 0), thickness = 3):
+        # cv2.drawContours(image, contors, contourldx, color, thickness, lineType,hierarchy, maxLevel,offset)
+        cv2.drawContours(imageName, contors, contourldx, color, thickness, lineType=-1)
+        # print(imageName)
+        cv2.imshow('drawContours', imageName)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def openCVdrawContours(self):
+        import cv2
+        import numpy as np
+
+        print(cv2.__version__)
+        image = cv2.imread('../images/test.png')
+        cv2.waitKey(0)
+
         # Grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -42,110 +145,72 @@ def getContours(image):
         # since findContours alters the imageName
         contours, hierarchy = cv2.findContours(edged,
                                                cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        return contours
-    except Exception as e:
-        print('Error in getContours', e)
-        return False
 
-"""
-contours @array
-contorIndex @num data contain index of contours
-imagePath @string path to the image file
-"""
-def cropContors(contours, contorIndex, imagePath):
-    try:
-        if contours:
-            idx = contorIndex
-            try:
-                image = cv2.imread(imagePath, 0)
-                mask = np.zeros_like(image)  # Create mask where white is what we want, black otherwise
-                cv2.drawContours(mask, contours, idx, 255, -1)  # Draw filled contour in mask
-                out = np.zeros_like(image)  # Extract out the object and place into output imageName
-                out[mask == 255] = image[mask == 255]
+        # cv2.imshow('Canny Edges After Contouring', edged)
+        # cv2.waitKey(0)
 
-                # Now crop
-                (y, x) = np.where(mask == 255)
-                (topy, topx) = (np.min(y), np.min(x))
-                (bottomy, bottomx) = (np.max(y), np.max(x))
-                out = out[topy:bottomy + 1, topx:bottomx + 1]
+        print("Number of Contours found = " + str(len(contours)))
 
-                return out
-            except Exception as e:
-                print('Error in cropContors imread',e)
-                return False
-        else:
-            return False
-    except Exception as e:
-        print('Error in cropContors main',e)
-        return False
+        # Draw all contours
+        # -1 signifies drawing all contours
+        # cv2.drawContours(imageName, contours[190], -1, (0, 255, 0), 3)
 
-def openCVdrawContours():
-    import cv2
-    import numpy as np
-
-    print(cv2.__version__)
-    image = cv2.imread('../images/test.png')
-    cv2.waitKey(0)
-
-    # Grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Find Canny edges
-    edged = cv2.Canny(gray, 30, 200)
-    cv2.waitKey(0)
-
-    # Finding Contours
-    # Use a copy of the imageName e.g. edged.copy()
-    # since findContours alters the imageName
-    contours, hierarchy = cv2.findContours(edged,
-                                           cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # cv2.imshow('Canny Edges After Contouring', edged)
-    # cv2.waitKey(0)
-
-    print("Number of Contours found = " + str(len(contours)))
-
-    # Draw all contours
-    # -1 signifies drawing all contours
-    # cv2.drawContours(imageName, contours[190], -1, (0, 255, 0), 3)
-
-    # print(contours[190])
-    # print(contours[190][0][0])
-    # # , contours[190][len(contours[190])][0])
-    # print(len(contours[190]))
-    # print(contours[190][len(contours[190])-1][0])
-    # cv2.imshow('Contours', imageName)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        # print(contours[190])
+        # print(contours[190][0][0])
+        # # , contours[190][len(contours[190])][0])
+        # print(len(contours[190]))
+        # print(contours[190][len(contours[190])-1][0])
+        # cv2.imshow('Contours', imageName)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
 
-    idx = 190
-    image = cv2.imread('../images/test.png', 0)
-    mask = np.zeros_like(image)  # Create mask where white is what we want, black otherwise
-    cv2.drawContours(mask, contours, idx, 255, -1)  # Draw filled contour in mask
-    out = np.zeros_like(image)  # Extract out the object and place into output imageName
-    out[mask == 255] = image[mask == 255]
+        idx = 190
+        image = cv2.imread('../images/test.png', 0)
+        mask = np.zeros_like(image)  # Create mask where white is what we want, black otherwise
+        cv2.obj.drawContours(mask, contours, idx, 255, -1)  # Draw filled contour in mask
+        out = np.zeros_like(image)  # Extract out the object and place into output imageName
+        out[mask == 255] = image[mask == 255]
 
-    # Now crop
-    (y, x) = np.where(mask == 255)
-    (topy, topx) = (np.min(y), np.min(x))
-    (bottomy, bottomx) = (np.max(y), np.max(x))
-    out = out[topy:bottomy + 1, topx:bottomx + 1]
+        # Now crop
+        (y, x) = np.where(mask == 255)
+        (topy, topx) = (np.min(y), np.min(x))
+        (bottomy, bottomx) = (np.max(y), np.max(x))
+        out = out[topy:bottomy + 1, topx:bottomx + 1]
 
-    cv2.imshow('Output', out)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-image = cv2.imread('../images/test.png')
-contors = getContours(image)
-for i in range(0, len(contors)):
-    outputImage = cropContors(getContours(image), i, '../images/test.png')
-    print(outputImage.shape)
-    height, width = outputImage.shape
-    if width == 33:
-        cv2.imshow('Output', outputImage)
+        cv2.imshow('Output', out)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+obj = imageProcessing()
+
+image = cv2.imread('../images/test.png')
+
+contors = obj.getContours(image)
+
+obj.drawContours(image,contors)
+for i in range(0, len(contors)):
+    outputImage = obj.cropContors(obj.getContours(image), i, '../images/test.png')
+    # print(outputImage.shape)
+    try:
+        height, width = outputImage.shape
+
+        # print('!' +getOcr(outputImage) + '!')
+        if obj.getValidOcr(outputImage):
+            confidence = pytesseract.image_to_data(outputImage, output_type='data.frame')
+            # print(type(confidence))
+            confidence =  confidence[confidence.conf != -1]
+            print(confidence)
+            # obj.drawContours(image,contors)
+            # cv2.imshow('Output', outputImage)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+    except Exception as e:
+        # print((e))
+        continue
+
+cv2.imshow('final', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 # ocrImage('test', 'png', 'test_ocr.txt')
 # print(pytesseract.image_to_string('../images/test.png'))
--- venv/main.py
+++ venv/main.py
@@ -9,6 +9,15 @@ class MainRPA:
 
         return FireFox(options)
 
+    def imageProcessing(self, options):
+        try:
+            os.chdir(options['imagProcessing']['path'])
+            from imageProcessing import imageProcessing
+            return True
+        except Exception as e:
+            print('error from imageProcessing', e)
+            return False
+
     def run(self, os, option, path):
         try:
             print('Oppening RPA')
@@ -35,17 +44,26 @@ class MainRPA:
 
             return False
 
+
 if __name__ == '__main__':
     try:
-        if len(sys.argv) == 3:
-            pathFile = open('path.json', 'r')
-            path = json.loads(pathFile.read())
-            pathFile.close()
+        pathFile = open('path.json', 'r')
+        path = json.loads(pathFile.read())
+        pathFile.close()
 
-            if MainRPA.run(MainRPA, sys.argv[1], sys.argv[2], path):
-                print('Process complete')
+        if len(sys.argv) == 4:
+            if sys.argv[3] == 'rpa':
+                if MainRPA.run(MainRPA, sys.argv[1], sys.argv[2], path):
+                    print('Process complete')
             else:
                 print('Process exit with error')
+        elif len(sys.argv) == 3:
+            print('imageProcessing', sys.argv)
+            path = path[sys.argv[1]]
+            if MainRPA.imageProcessing(MainRPA, path):
+                print('image processing complete')
+            else:
+                print('image processing faild')
         else:
             print('Error: Proper argument need to run the program')
     except Exception as e:
