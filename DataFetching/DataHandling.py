37bfc9216101c97163a5ab98da17b989b123e948
jassim <incubator482@gmail.com>
2020-01-30 10:12:43 +0530

    Documentation DataHandling

--- DataFetching/DataHandling.py
+++ DataFetching/DataHandling.py
@@ -7,15 +7,12 @@ from ExceptionHandling.GeneralExceptionHandling import GeneralExceptionHandling
 class DataHandling:
     def __init__(self, path, group):
         self.path = path
-        # self.dbOption = GeneralExceptionHandling.getJsonDataRecurssive(GeneralExceptionHandling, 'DB', self.path)
         self.dbOptions = path
-        # self.LocatorDirectory = GeneralExceptionHandling.getJsonDataRecurssive(GeneralExceptionHandling,
-        #                                                                   'DataFetching,filesPath', self.path)
-        # self.getConnection()
         self.layerConnectorName = group
         self.locatorId = 'locator_id'
         self.locatorData = 'locatorData'
         self.layerConnectorName = 'datafeth_locator_'
+        print(path)
 
     def __del__(self):
         try:
@@ -30,22 +27,35 @@ class DataHandling:
             except:
                 pass
             self.mydb = mysql.connector.connect(host=self.dbOptions['mysql']['default']['HOST'], user=self.dbOptions['mysql']['default']['USER'], password=self.dbOptions['mysql']['default']['PASSWORD'], database=self.dbOptions['mysql']['default']['DB'])
-            # print(self.mydb)
+
             return True
         except Exception as e:
             print('error in mysql', e)
             return False
 
-    def getTableFromDB(self, layerName):
+    def getTableFromDB(self, layerName) -> bool:
+        """
+        Layer name corresponding table from db is checked
+        :param layerName: Name of layer
+        :var tableName: Converted corresponding mysql table name
+        :return: True if exist else False
+        :todo: Unfinished
+        """
         self.getConnection()
         tableName = self.layerConnectorName+layerName
         if self.checkTableExists(tableName):
-            print('table exist')
+            return True
         else:
             return False
-        return True
 
-    def createLayer(self, layerName):
+
+    def createLayer(self, layerName) -> bool:
+        """
+        Creating table with structure for layer name
+        :param layerName: Name of layer
+        :var tableName: Converted corresponding mysql table Name
+        :return:
+        """
         try:
             tableName = self.layerConnectorName + layerName
             self.getConnection()
@@ -60,6 +70,12 @@ class DataHandling:
             return False
 
     def deleteLayer(self, layerName):
+        """
+        Layer name exist in db then delete
+        :param layerName: Name of layer
+        :var tableName: Converted corresponding mysql table name
+        :return: If no database error occure means Return True else False
+        """
         try:
             tableName = self.layerConnectorName + layerName
             sql = f"drop table if exists {tableName};"
@@ -73,12 +89,23 @@ class DataHandling:
             print('error in deleteLayer in DataHandling', e)
             return False
 
-    def checkLayerExists(self, layerName):
+    def checkLayerExists(self, layerName) -> bool:
+        """
+        Confirming a layer corresponding table exists
+        :param layerName: Layer name
+        :var tableName: Converting corresponding name for db table name
+        :return: True if table exist else False
+        """
         self.getConnection()
         tableName = self.layerConnectorName + layerName
         return self.checkTableExists(tableName)
 
-    def checkTableExists(dbcon, tablename):
+    def checkTableExists(dbcon, tablename) -> bool:
+        """
+        Directly checking tablename in DB
+        :param tablename:
+        :return: True if exist else False
+        """
         dbcur = dbcon.mydb.cursor()
         dbcur.execute("""
             SELECT COUNT(*)
@@ -95,6 +122,11 @@ class DataHandling:
         return False
 
     def getLayerDataCount(self, layerName):
+        """
+        Calculating overal row count corresponging to locator id
+        :param layerName: Name of layer
+        :return: Number if table exist else False
+        """
         if self.checkLayerExists(layerName):
             tableName = self.layerConnectorName + layerName
             self.getConnection()
@@ -108,7 +140,14 @@ class DataHandling:
             return False
 
 
-    def checkSimilarRecord(self, layerName, locatorId, locatorData):
+    def checkSimilarRecord(self, layerName, locatorId, locatorData) -> bool:
+        """
+        If duplicate records there then return False
+        :param layerName: Name of layer
+        :param locatorId: locatorid field data in mysql table
+        :param locatorData: Data in mysql
+        :return: True if exist else False
+        """
         tableName = self.layerConnectorName + layerName
         sql = f"select count({self.locatorId}) from {tableName} where {self.locatorId} = '{locatorId}' and {self.locatorData} = '{locatorData}'"
         # print(sql)
@@ -125,7 +164,15 @@ class DataHandling:
         else:
             return False
 
-    def insertRecord(self, layerName, locatorId, locatorData):
+    def insertRecord(self, layerName, locatorId, locatorData) -> bool:
+        """
+        Directly inserting a value to the mysql table
+        :param layerName: Name of layer
+        :param locatorId: Data
+        :param locatorData: Data
+        :var tableName: Corresponding mysql table name
+        :return: True if success else False
+        """
         try:
             tableName = self.layerConnectorName + layerName
             self.getConnection()
@@ -142,7 +189,15 @@ class DataHandling:
             print('error in insertRecord in DataHandling', e)
             return False
 
-    def insertLayer(self, layerName, locatorId, locatorData):
+    def insertLayer(self, layerName, locatorId, locatorData) -> bool:
+        """
+        Inserting a layer/data, if layer exists then insert only else create layer then insert
+        If similar record then return False
+        :param layerName: Name of layer
+        :param locatorId: Data
+        :param locatorData: Data
+        :return: True if inserted else False
+        """
         try:
             if self.checkLayerExists(layerName):
                 if not self.checkSimilarRecord(layerName, locatorId, locatorData):
@@ -151,7 +206,6 @@ class DataHandling:
                 else:
                     return False
             else:
-                print('No layer exist')
                 if self.createLayer(layerName):
                     self.insertRecord(layerName, locatorId, locatorData)
                     return True
@@ -162,11 +216,21 @@ class DataHandling:
             return False
 
     def generateLocatorFromTableField(self, layerName, locatorId, schemaName, tableName, fieldNameFromTable):
+        """
+        Direct layer map from a specific table data field
+        :param layerName: Name of layer
+        :param locatorId: Data
+        :param schemaName: Schema name
+        :param tableName: Name of table
+        :param fieldNameFromTable: Field name
+        :var dataFrameFromDB: Pandas dataframe
+        :return:
+        """
         try:
             dataFrameFromDB = self.getDataFromDBAsDF(schemaName, tableName, fieldNameFromTable)
-            dataFrameFromDB[self.locatorId]=locatorId
 
             if not dataFrameFromDB.empty:
+                dataFrameFromDB[self.locatorId] = locatorId
                 sql = f"create table {self.layerConnectorName+layerName} ({self.locatorId} varchar(200), {self.locatorData} varchar(500));"
                 print(sql)
 
@@ -178,18 +242,28 @@ class DataHandling:
             print('error in generateLocatorFromTableField in DataHandling', e)
             return pd.DataFrame()
 
-    def getDataFromDBAsDF(self, dataBaseForFetching, tableName, filedName):
-            try:
-                dbOptions = self.dbOptions['mysql']['default']
-                engine = create_engine(
-                    f'mysql+pymysql://{dbOptions["USER"]}:{dbOptions["PASSWORD"]}@{dbOptions["HOST"]}/{dataBaseForFetching}')
+    def getDataFromDBAsDF(self, dataBaseForFetching, tableName, filedName) -> pd.DataFrame:
+        """
+        Get data frame from Database table as dataframe format
+        :param dataBaseForFetching: Schema name
+        :param tableName: Name of table
+        :param filedName: Name of mysql field name
+        :var dbOptions: json data with database credentials
+        :var engine: sqlalchmy mysql connection
+        :var df: DataFrame object
+        :return: DataFrame
+        """
+        try:
+            dbOptions = self.dbOptions['mysql']['default']
+            engine = create_engine(
+                f'mysql+pymysql://{dbOptions["USER"]}:{dbOptions["PASSWORD"]}@{dbOptions["HOST"]}/{dataBaseForFetching}')
 
-                sql = f"select distinct {filedName} from {tableName}"
-                where = f" where {filedName} is not null"
+            sql = f"select distinct {filedName} from {tableName}"
+            where = f" where {filedName} is not null"
 
-                df = pd.read_sql(sql+where , con=engine)
+            df = pd.read_sql(sql+where , con=engine)
 
-                return df
-            except Exception  as e:
-                # print('error in getLocatorFromDb in LocatorFromDB', e)
-                 return pd.DataFrame()
+            return df
+        except Exception  as e:
+            # print('error in getLocatorFromDb in LocatorFromDB', e)
+             return pd.DataFrame()
